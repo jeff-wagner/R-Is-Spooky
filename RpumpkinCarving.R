c7e65791684994b06242f68ee5924a58daa77025
jeff-wagner <60358063+jeff-wagner@users.noreply.github.com>
2020-10-30 23:00:04 -0600

    Update RpumpkinCarving.R

--- RpumpkinCarving.R
+++ RpumpkinCarving.R
@@ -6,25 +6,31 @@
 # -------------------- Original by Allan Roberts -------------------#
 # ------------------------------------------------------------------#
 
-
+# Set the background and build our pumpkin
 par(bg=1, mar=c(0,0,0,0))
 plot(c(0,0), cex=0, xlim=c(-1,1), ylim=c(1.5,0))
 X <- runif(500,-1,1)
 Y <- runif(500,0,1)
 M <- rchisq(500,1)/20
 points(Y~X, cex=M, pch=19, col= "white") # let's make some stars to set the mood
-points(0,1.1, pch=19, col= "orange",cex=20) # now make our pumpkin 
-polygon(c(-0.03, -0.08, -0.02, 0.03), c(0.86, 0.76, 0.78, 0.86),  # don't forget the stem!
+points(0,1.1, pch=19, col= "orange",cex=30) # now make our pumpkin 
+polygon(c(-0.06, -0.08, -0.02, 0.06), c(0.76, 0.59, 0.61, 0.76),  # don't forget the stem!
         col= "darkgreen",
         border= "darkgreen",
         lwd=3, lty= "solid")
 
-# Use the mouse to carve the eyes and nose, clicking at 3 points for each polygon
-for (i in 1:3){
-  polygon(locator(3),col=7)
+## NOTE: You can change the number of points used for each polygon by adjusting the value within the locator() function ##
+
+
+# Use the mouse to carve the eyes, clicking at 4 points for each polygon
+for (i in 1:2){
+  polygon(locator(4),col=7)
 }
 
-# Now carve the mouth, clicking at 15 points (you can adjust the number of points inside the locator() function below)
-polygon(locator(15), col = 7)
+# Then, carve the nose, clicking at 3 points
+polygon(locator(3), col = 7)
+
+# Finally, carve the mouth, clicking at 10 points
+polygon(locator(10), col = 7)
 
 # Congratulations, you just carved your first virtual pumpkin with R! You're being a nerd for Halloween!
